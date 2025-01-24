3e14637698b24d3c7f58a6eed871b85f24ad1468
Gaia Zanella <zanella.gaia@gmail.com>
2025-01-24 16:54:18 +0100

    terremoti_download

--- script/mseed_terremoti/terremoti_download.py
+++ script/mseed_terremoti/terremoti_download.py
@@ -14,11 +14,13 @@ channel = ['*H*']
 
 
 # Client pour récupérer les données
-client = Client(db)
+client = Client(db) °)))))))))))°°°°°°°
 ti = UTCDateTime("2020-10-21T23:00:00.000")
 tf = ti + (60 * 6 * 1)  # 1 heure de données
 
 # Récupérer les données pour les deux stations
 st1 = client.get_waveforms(network=net[0], station=stz[0], location="", channel=channel[0], starttime=ti, endtime=tf)
 st1.write('/home/gaia/Documents/gaia_scripts/script/20201021_M5.2.mseed')
+-_-_=(())
 st1.plot()
+===
