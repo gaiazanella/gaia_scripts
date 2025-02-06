8605ed5780087bfdba71e2666ccf7bf5b93e7fa5
Gaia Zanella <zanella.gaia@gmail.com>
2025-02-06 16:21:35 +0100

    modif com code english

--- miniseed_terremoti/calcul_rapport_spectrale_20201023.py
+++ miniseed_terremoti/calcul_rapport_spectrale_20201023.py
@@ -8,9 +8,8 @@ stream_station.detrend("demean") #met le signal autour de zéro
 stream_station.detrend("linear") 
 print(stream_station)
 
-win = 30 #60 #window of 60 seconds, 1 min
+win = 30 #30sec
 
-# Choisir la trace de chaque station (par exemple, la première trace de chaque stream)
 trace_station_stra = stream_station[8]
 print(trace_station_stra)
 trace_station_stre = stream_station[14]
@@ -23,13 +22,13 @@ print(trace_station_strg)
 sampling_rate = trace_station_stra.stats.sampling_rate
 samples = int(win * sampling_rate)
 
-window = np.ones(samples) / samples # crée un tableau rempli de 1 de taille égale à window_size ; chaque case devient 1/window_size : moyenne uniforme
+window = np.ones(samples) / samples 
 smoothed_signal_stra = np.convolve(trace_station_stra.data, window, mode='same')
 smoothed_signal_stre = np.convolve(trace_station_stre.data, window, mode='same')
 smoothed_signal_strc = np.convolve(trace_station_strg.data, window, mode='same')
 smoothed_signal_strg = np.convolve(trace_station_strg.data, window, mode='same')
 
-# Récupérer les données de chaque station
+# data station
 data_station_stra = trace_station_stra.data
 data_station_stre = trace_station_stre.data
 data_station_strc = trace_station_strc.data
@@ -39,26 +38,24 @@ data_station_strc=data_station_strc*1.00**(-6)/1200 ## STRC
 data_station_stre=data_station_stre*3.20**(-6)/800 ## STRE
 data_station_strg=data_station_strg*1.00**(-6)/1200 ## STRG
 
-
-# Calcul de la FFT pour chaque station
+# FFT calcul for each station
 spectrum_station_stra = np.fft.fft(data_station_stra)
 spectrum_station_stre = np.fft.fft(data_station_stre)
 spectrum_station_strc = np.fft.fft(data_station_strc)
 spectrum_station_strg = np.fft.fft(data_station_strg)
 
-# Calcul des amplitudes spectrales
+# Spectral amplitude calcul for each station
 amplitude_station_stra = np.abs(spectrum_station_stra)
 amplitude_station_stre = np.abs(spectrum_station_stre)
 amplitude_station_strc = np.abs(spectrum_station_strc)
 amplitude_station_strg = np.abs(spectrum_station_strg)
 
-# Calcul des fréquences associées
+# frequencies associated calcul
 n = len(data_station_stra)
 frequencies = np.fft.fftfreq(n, d=1/sampling_rate)
 
-# Ne garder que les fréquences positives
+# keep positive frequencies
 positive_frequencies = frequencies[frequencies > 0]
-#useful_frequencies = positive_frequencies[(positive_frequencies > 8) & (positive_frequencies < 15)]
 amplitude_station_stra = amplitude_station_stra[frequencies > 0]
 amplitude_station_stre = amplitude_station_stre[frequencies > 0]
 amplitude_station_strc = amplitude_station_strc[frequencies > 0]
@@ -66,12 +63,11 @@ amplitude_station_strg = amplitude_station_strg[frequencies > 0]
 
 amplitude_station_ref = amplitude_station_strg
 
-# Calcul du rapport spectral (station 1 / station 2)
+# ratio spectral calcul
 spectral_ratio_e = amplitude_station_stre / amplitude_station_ref
 spectral_ratio_c = amplitude_station_strc / amplitude_station_ref
 spectral_ratio_g = amplitude_station_strg / amplitude_station_ref
 spectral_ratio_a = amplitude_station_stra / amplitude_station_ref
-#print(spectral_ratio)
 
 # Affichage du rapport spectral
 plt.figure(figsize=(10, 6))
