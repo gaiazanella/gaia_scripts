af5334b7233a80b794a9bf5ef975a0c189dc9cd6
Gaia Zanella <zanella.gaia@gmail.com>
2025-03-03 15:27:33 +0100

    lds catalog  compo z

--- miniseed_terremoti/landslide_download.py
+++ miniseed_terremoti/landslide_download.py
@@ -19,10 +19,10 @@ client = Client(db)
 #tf = ti + (60 * 2 * 1)  # 20 min de données
 
 # Heure de début précise à 19:02
-ti = UTCDateTime("2024-08-01T19:41:00.000")
+ti = UTCDateTime("2020-06-26T04:00:00.000")
 
 # Heure de fin précise à 19:04
-tf = UTCDateTime("2024-08-01T19:48:00.000")
+tf = UTCDateTime("2020-06-26T06:30:00.000")
 
 # Récupérer les données pour les deux stations
 st1 = client.get_waveforms(network=net[0], station=stz[0], location="", channel=channel[0], starttime=ti, endtime=tf)
@@ -39,12 +39,16 @@ for trace in st1:
 # Afficher le stream prétraité
 st1.plot()
 
+st1.write('/home/gaia/Documents/mseed_landslide/mseed_lds_catalog/20200626_04.15.mseed')
+
+
+########################
 #st1.merge()
 #st1.detrend("demean") #met le signal autour de zéro 
 #st1.detrend("linear") 
 #print(st1)
 
-#st1.write('/home/gaia/Documents/mseed_landslide/mseed_landslide_test/20200323_10.08.mseed')
+
 #st1.plot()
 
 # Chemin vers ton fichier MiniSEED
@@ -59,3 +63,19 @@ st1.plot()
 # Optionnel : Tracer le premier trace pour vérifier visuellement
 #st[0].plot()
 
+##################
+ffff
+
+
+# Lire le fichier MiniSEED
+traces = read("/home/gaia/Documents/mseed_landslide/mseed_lds_catalog/20200206_15.30.mseed")
+print(traces)
+# Afficher un graphique de la première trace
+traces[0].plot()
+
+# Afficher les informations détaillées de la première trace
+print("Station:", traces[0].stats.station)
+print("Canal:", traces[0].stats.channel)
+print("Temps de début:", traces[0].stats.starttime)
+print("Temps de fin:", traces[0].stats.endtime)
+print("Données:", traces[0].data)
